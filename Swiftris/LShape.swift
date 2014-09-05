603a0d42d65bed0497e315415a5613619dcac270
yhyuan <yhyuan@gmail.com>
2014-09-05 11:07:34 -0400

    Update LShape.swift

--- Swiftris/LShape.swift
+++ Swiftris/LShape.swift
@@ -8,24 +8,52 @@
 
 import Foundation
 class LShape: Shape {
-	/*
-		
-	*/
-	override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
-		return [
-			Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.OneEighty: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.Ninety: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.TwoSeventy: [(0, 0), (1, 0), (0, 1), (1, 1)]
-		]
-	}
+    /*
 
-	override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
-		return [
-			Orientation.Zero: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.OneEighty: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.Ninety: [[blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.TwoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
-		]
-	}
+    Orientation 0
+
+        | 0•|
+        | 1 |
+        | 2 | 3 |
+
+    Orientation 90
+
+          •
+    | 2 | 1 | 0 |
+    | 3 |
+
+    Orientation 180
+
+    | 3 | 2•|
+        | 1 |
+        | 0 |
+
+    Orientation 270
+
+          • | 3 |
+    | 0 | 1 | 2 |
+
+    • marks the row/column indicator for the shape
+
+    Pivots about `1`
+
+    */
+
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [ (0, 0), (0, 1),  (0, 2),  (1, 2)],
+            Orientation.Ninety:     [ (1, 1), (0, 1),  (-1,1), (-1, 2)],
+            Orientation.OneEighty:  [ (0, 2), (0, 1),  (0, 0),  (-1,0)],
+            Orientation.TwoSeventy: [ (-1,1), (0, 1),  (1, 1),   (1,0)]
+        ]
+    }
+
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[ThirdBlockIdx]]
+        ]
+    }
 }
