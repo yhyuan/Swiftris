27097fcf16364ff61aa157e760a40231cc224a03
yhyuan <yhyuan@gmail.com>
2014-09-05 11:09:28 -0400

    Update SquareShape.swift

--- Swiftris/SquareShape.swift
+++ Swiftris/SquareShape.swift
@@ -8,9 +8,18 @@
 
 import Foundation
 class SquareShape: Shape {
-	/*
-		
-	*/
+    /*
+// #1
+         | 0•| 1 |
+         | 2 | 3 |
+
+     • marks the row/column indicator for the shape
+
+     */
+
+     // The square shape will not rotate
+
+// #2
 	override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
 		return [
 			Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
