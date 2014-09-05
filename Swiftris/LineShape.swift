1672c972782c5187c9839fb9f69ec4fa1420d21d
yhyuan <yhyuan@gmail.com>
2014-09-05 11:07:04 -0400

    Update LineShape.swift

--- Swiftris/LineShape.swift
+++ Swiftris/LineShape.swift
@@ -8,24 +8,39 @@
 
 import Foundation
 class LineShape: Shape {
-	/*
-		
-	*/
-	override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
-		return [
-			Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.OneEighty: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.Ninety: [(0, 0), (1, 0), (0, 1), (1, 1)],
-			Orientation.TwoSeventy: [(0, 0), (1, 0), (0, 1), (1, 1)]
-		]
-	}
+ /*
+        Orientations 0 and 180:
 
-	override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
-		return [
-			Orientation.Zero: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.OneEighty: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.Ninety: [[blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
-			Orientation.TwoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
-		]
-	}
+            | 0•|
+            | 1 |
+            | 2 |
+            | 3 |
+
+        Orientations 90 and 270:
+
+        | 0 | 1•| 2 | 3 |
+
+    • marks the row/column indicator for the shape
+
+    */
+
+    // Hinges about the second block
+
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.Ninety:     [(-1,0), (0, 0), (1, 0), (2, 0)],
+            Orientation.OneEighty:  [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.TwoSeventy: [(-1,0), (0, 0), (1, 0), (2, 0)]
+        ]
+    }
+
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[FourthBlockIdx]],
+            Orientation.Ninety:     blocks,
+            Orientation.OneEighty:  [blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: blocks
+        ]
+    }
 }
